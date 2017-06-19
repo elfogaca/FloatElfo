1c5c6c86afef9490541415b109a690d46268f611
elfogaca <elfogaca@gmail.com>
2017-06-19 19:55:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/androidfloatingbuttonexample/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.androidfloatingbuttonexample;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+//import android.content.DialogInterface;
+import android.app.AlertDialog;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_launcher_star);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        */
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Buttom flutuante clicado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.about) {
+            TextView content = (TextView) getLayoutInflater().inflate(R.layout.about_view, null);
+            content.setMovementMethod(LinkMovementMethod.getInstance());
+            content.setText(Html.fromHtml(getString(R.string.about_body)));
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.about)
+                    .setView(content)
+                    .setInverseBackgroundForced(true)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
